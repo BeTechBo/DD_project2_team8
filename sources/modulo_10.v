d9f8b9bd1af30f73e600e1d7d388e795f9ba897e
omarsaqr12 <omar_saqr@aucegypt.edu>
2024-05-19 08:22:00 +0300

    Final Release

--- sources/modulo_10.v
+++
@@ -1,8 +0,0 @@
-module modulo_10_counter(input clk,input reset,input enable,output [3:0] count);
-integer n=10;
-wire clk_out;   
-
-clockDivider #(3000000000) j1(.clk(clk),.rst (reset),.clk_out (clk_out));
-exp1#(4,10) j2(.clk(clk_out),.reset(reset),.enable(enable),.count(count));
-
-endmodule
--- sources/modulo_6.v
+++
@@ -1,8 +0,0 @@
-module modulo_6_counter(input clk,input reset,input enable,output [3:0] count);
-integer n=6;
-wire clk_out;   
-
-clockDivider #(3000000000) l1(.clk(clk),.rst (reset),.clk_out (clk_out));
-exp1#(3,6) l2(.clk(clk_out),.reset(reset),.enable(enable),.count(count));
-
-endmodule
