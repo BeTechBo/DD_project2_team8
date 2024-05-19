838c7e43e9d2c8595d96d265100e9658c9054e05
omarsaqr12 <125351767+omarsaqr12@users.noreply.github.com>
2024-05-19 07:16:49 +0300

    Update rising_edge_detector.v
    rising_edge_detector after documentation

--- sources/rising_edge_detector.v
+++ sources/rising_edge_detector.v
@@ -3,7 +3,7 @@
 // Company: 
 // Engineer: 
 // 
-// Create Date: 11/21/2023 06:30:35 PM
+// Create Date: 7/5/2024 06:30:35 PM
 // Design Name: 
 // Module Name: re_dectetor
 // Project Name: 
@@ -19,50 +19,44 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-//module fsm(input clk, rst, w, output z);
-//reg [1:0] state, nextState;
-//parameter [1:0] A=2'b00, B=2'b01, C=2'b10; // States Encoding
-//// Next state generation (combinational logic)
-//always @ (w or state)
-//case (state)
-//A: if (w==0) nextState = A;
-// else nextState = B;
-//B: if (w==0) nextState = A;
-// else nextState = C;
-//C: if (w==0) nextState = A;
-// else nextState = C;
-//default: nextState = A;
-//endcase
-//// State register
-//// Update state FF's with the triggering edge of the clock
-//always @ (posedge clk or posedge rst) begin
-//if(rst) state <= A;
-//else state <= nextState;
-//end
-//// output generation (combinational logic)
-//assign z = (rst ==0 && state == B);
-//endmodule
+/***********************
+*
+* Module: fsm.v
+* Project: Digital Design (Project 2)
 
-module fsm(input clk, rst, w, output z);
-reg [1:0] state, nextState;
-parameter [1:0] A=2'b00, B=2'b01, C=2'b10; // States Encoding
-// Next state generation (combinational logic)
-always @ (w or state)
-case (state)
-A: if (w==0) nextState = A;
-else nextState = B;
-B: if (w==0) nextState = A;
-else nextState = C;
-C: if (w==0) nextState = A;
-else nextState = C;
-default: nextState = A;
-endcase
-// State register
-// Update state FF's with the triggering edge of the clock
-always @ (posedge clk or posedge rst) begin
-if(rst) state <= A;
-else state <= nextState;
-end
-// output generation (combinational logic)
-assign z = (rst ==0 && state == B);
+* Author: Adham Ali/ adhamahmed804@aucegypt.edu 
+          Omar Saqr/ omar_saqr@aucegypt.edu 
+          Ebram Thabet / ebram_raafat@aucegypt.edu 
+          
+ *Description:  Finite State Machine (FSM) for state transition based on input signal 'w'         
+************************/
+
+
+module fsm(
+    input clk, 
+    rst, w, 
+    output z
+);
+    
+    reg [1:0] state, nextState;
+    parameter [1:0] A=2'b00, B=2'b01, C=2'b10; // States Encoding
+    // Next state generation (combinational logic)
+    always @ (w or state)
+    case (state)
+    A: if (w==0) nextState = A;
+    else nextState = B;
+    B: if (w==0) nextState = A;
+    else nextState = C;
+    C: if (w==0) nextState = A;
+    else nextState = C;
+    default: nextState = A;
+    endcase
+    // State register
+    // Update state FF's with the triggering edge of the clock
+    always @ (posedge clk or posedge rst) begin
+    if(rst) state <= A;
+    else state <= nextState;
+    end
+    // output generation (combinational logic)
+    assign z = (rst ==0 && state == B);
 endmodule
